a0f1f9d8e9203136217767204e3a90420ddeff08
william <william-gin1@hotmail.com>
2020-11-28 17:01:04 -0500

    many things

---
+++ Assets/Scripts/Progession.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progession
+{
+    static public string key1 = "key1";
+    static public string key2 = "key2";
+
+    static public List<string> inventory = new List<string>();
+}
