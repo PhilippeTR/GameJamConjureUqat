5dcf288dafdaab92617d2f793b7de89fa281df59
Jim <jimmyetlim@gmail.com>
2020-11-28 15:37:21 -0500

    -tmp

---
+++ Assets/Scripts/Door/DoorAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Bytes;
+
+public class DoorAction : Action
+{
+    public bool isNear=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNear && Input.GetKeyDown("e")) {
+            Debug.Log("Activated");
+        }
+    }
+    public void executeEnter(GameObject g)
+    {
+        isNear = true;
+        EventManager.Dispatch(EventNames.interactionTextUpdate, new StringDataBytes("E - interact"));
+    }
+    public void executeStay(GameObject g)
+    {
+
+    }
+    public void executeLeave(GameObject g)
+    {
+        isNear = false;
+        EventManager.Dispatch(EventNames.interactionTextUpdate, new StringDataBytes(""));
+    }
+}
