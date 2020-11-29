d1f1b3275e9cbdebb1ba048719ec979ff98b5426
william <william-gin1@hotmail.com>
2020-11-29 15:16:05 -0500

    anim intro

---
+++ Assets/FinJeu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinJeu : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "joueur")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("TheEnd");
+        }
+    }
+}
